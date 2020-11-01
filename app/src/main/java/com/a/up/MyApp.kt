a32d381bc1c2788aadef0296a7a2f6f1a224636d
amirhossein.ghafourian <amhgh1377@gmail.com>
2020-11-01 13:13:09 +0330

    working on hilt

---
+++ app/src/main/java/com/a/up/MyApp.kt
@@ -0,0 +1,36 @@
+package com.a.up
+
+import android.app.Application
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+import com.facebook.flipper.plugins.sharedpreferences.SharedPreferencesFlipperPlugin
+import com.facebook.soloader.SoLoader
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApp : Application() {
+
+    companion object {
+//        val networkFlipperPlugin = NetworkFlipperPlugin()
+        lateinit var publicApp: Application
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        publicApp = this
+//        SoLoader.init(this, false)
+//        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+//            val client = AndroidFlipperClient.getInstance(this)
+//            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+//            client.addPlugin(DatabasesFlipperPlugin(this))
+//            client.addPlugin(networkFlipperPlugin)
+//            client.addPlugin(SharedPreferencesFlipperPlugin(this,"setting"))
+//
+//            client.start()
+//        }
+    }
+}
