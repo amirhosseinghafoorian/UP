1a11d738e59d58295529ee74b2b747926049dcf9
amirhossein.ghafourian <amhgh1377@gmail.com>
2020-11-01 09:02:11 +0330

    ui changes

--- app/src/main/java/com/a/up/home/ui/SplashFragment.kt
+++ app/src/main/java/com/a/up/home/ui/SplashFragment.kt
@@ -1,18 +1,35 @@
 package com.a.up.home.ui
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.a.up.R
+
 class SplashFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+//        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+//            activity?.window?.decorView?.systemUiVisibility =
+//                (View.SYSTEM_UI_FLAG_IMMERSIVE or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN)
+//        }
         return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val handler = Handler()
+        handler.postDelayed({
+
+            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToLoginFragment())
+        }, 3000)
     }
 
 }
--- app/src/main/java/com/a/up/user/ui/LoginFragment.kt
+++ app/src/main/java/com/a/up/user/ui/LoginFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.a.up.R
+import kotlinx.android.synthetic.main.fragment_login.*
+
 class LoginFragment : Fragment() {
 
     override fun onCreateView(
@@ -13,5 +16,17 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        login_btn.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToHomeHostFragment())
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        activity?.window?.decorView?.systemUiVisibility = 0
     }
 }
