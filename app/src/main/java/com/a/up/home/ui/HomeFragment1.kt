f576144e177aa87eb8dadea28eef76d15fa929ee
amirhossein.ghafourian <amhgh1377@gmail.com>
2020-11-04 09:23:14 +0330

    Epoxy Done

--- app/src/main/java/com/a/up/home/ui/HomeFragment1.kt
+++ app/src/main/java/com/a/up/home/ui/HomeFragment1.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.a.up.R
+import com.a.up.listItem
+import kotlinx.android.synthetic.main.fragment_home1.*
+
 class HomeFragment1 : Fragment() {
 
     override fun onCreateView(
@@ -13,6 +16,28 @@ class HomeFragment1 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_home1, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fillRecycler()
+    }
+
+    private fun fillRecycler() {
+        val items = mutableListOf<String>()
+        repeat(100) {
+            items.add("user ")
+        }
+
+        recyclerView.withModels {
+            items.forEachIndexed { pos, model ->
+                listItem {
+                    id(pos)
+                    username(model + pos.toString())
+                }
+            }
+        }
     }
 
 }
